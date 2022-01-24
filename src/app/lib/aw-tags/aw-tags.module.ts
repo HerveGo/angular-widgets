436fb8e912ede2d694b53dc570c537bf2f6e41b9
Hervé Gomanne <94847592+HerveGo@users.noreply.github.com>
2022-01-24 17:01:53 +0100

    animations

--- src/app/lib/aw-tags/aw-tags.module.ts
+++ src/app/lib/aw-tags/aw-tags.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AwTagsComponent } from './aw-tags.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AwTagsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule
   ],
   exports: [AwTagsComponent]
 })
