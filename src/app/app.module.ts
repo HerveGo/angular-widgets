91defdff103945d814969d972b342a172bdafc06
Hervé Gomanne <94847592+HerveGo@users.noreply.github.com>
2022-01-26 09:42:36 +0100

    cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AwBlogComponent } from './components/aw-blog/aw-blog.component';
 import { AwBlogFormComponent } from './components/aw-blog-form/aw-blog-form.component';
 import { FormsModule } from '@angular/forms';
 import { AwBlogArticleComponent } from './components/aw-blog-article/aw-blog-article.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { AwBlogArticleComponent } from './components/aw-blog-article/aw-blog-art
     AwBlogComponent,
     AwBlogFormComponent,
     AwBlogArticleComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
