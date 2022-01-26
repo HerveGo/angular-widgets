4b6470f01173849887e38c8a40e0761729cc0428
Hervé Gomanne <hgomanne@gmail.com>
2022-01-26 22:41:44 +0100

    class crypto

--- src/app/lib/crypto-market.ts
+++ src/app/lib/crypto-market.ts
@@ -1,28 +1,28 @@
 export class CryptoMarket {
-    id:string =  'bitcoin'
-    symbol:string =  'btc'
-    name:string =  'Bitcoin'
-    image:string =  'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579'
-    current_price:number =  33685,
-    market_cap:number =  638003647811,
-    market_cap_rank:number =  1,
-    fully_diluted_valuation:number =  707376200186,
-    total_volume:number =  41321412747,
-    high_24h:number =  34114,
-    low_24h:number =  32354,
-    price_change_24h:number =  1072.44,
-    price_change_percentage_24h:number =  3.28848,
-    market_cap_change_24h:number =  21329655375,
-    market_cap_change_percentage_24h:number =  3.45882,
-    circulating_supply:number =  18940525,
-    total_supply:number =  21000000,
-    max_supply:number =  21000000,
-    ath:number =  59717,
-    ath_change_percentage:number =  -43.5486,
-    ath_date:number =  2021-11-10T14:number = 24:number = 11.849Z,
-    atl:number =  51.3,
-    atl_change_percentage:number =  65615.46524,
-    atl_date:number =  2013-07-05T00:number = 00:number = 00.000Z,
-    roi:number =  null,
-    last_updated:number =  2022-01-26T15:number = 53:number = 51.894Z
+    id:string = 'bitcoin';
+    symbol:string = 'btc';
+    name:string = 'Bitcoin';
+    image:string = 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579';
+    current_price:number = 33685;
+    market_cap:number = 638003647811;
+    market_cap_rank:number = 1;
+    fully_diluted_valuation:number = 707376200186;
+    total_volume:number = 41321412747;
+    high_24h:number = 34114;
+    low_24h:number = 32354;
+    price_change_24h:number = 1072.44;
+    price_change_percentage_24h:number = 3.28848;
+    market_cap_change_24h:number = 21329655375;
+    market_cap_change_percentage_24h:number = 3.45882;
+    circulating_supply:number = 18940525;
+    total_supply:number = 21000000;
+    max_supply:number = 21000000;
+    ath:number = 59717;
+    ath_change_percentage:number = -43.5486;
+    ath_date: string = '2021-11-10T14:24:11.849Z';
+    atl: string = '51.3';
+    atl_change_percentage: string = '65615.46524';
+    atl_date: string = '2013-07-05T00:00:00.000Z';
+    roi: string|null = null;
+    last_updated: string = '2022-01-26T15:53:51.894Z';
 }
