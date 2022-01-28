e91e4af44f7f380f0e50d3abe41b66d0ead29780
Hervé Gomanne <hgomanne@gmail.com>
2022-01-28 16:47:49 +0100

    ng test
    see aw-tags

--- src/app/lib/aw-tags/aw-tags.component.spec.ts
+++ src/app/lib/aw-tags/aw-tags.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { AwTagsComponent } from './aw-tags.component';
 
@@ -22,4 +24,27 @@ describe('AwTagsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("shouldn't have any tags", () => {
+    expect(component.tags.length).toEqual(0);
+  });
+
+  it("should add tag", () => {
+    const input: DebugElement = fixture.debugElement.query(By.css("input"));
+    expect(input).toBeTruthy();
+    input.nativeElement.value = 'Tag 1';
+    input.triggerEventHandler("keyup.enter", { target: input.nativeElement });
+    fixture.detectChanges();
+
+    spyOn(fixture.componentInstance, "add");
+    fixture.componentInstance.add(new Event("keyup.enter"));
+
+    expect(component.tags.length).toEqual(1);
+    expect(component.tags[0] = "tag 1");
+    expect(fixture.componentInstance.add).toHaveBeenCalled();
+    expect(fixture.componentInstance.add).toHaveBeenCalledTimes(1);
+
+  });
+
+
 });
